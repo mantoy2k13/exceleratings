c447a7712d2e8bb4350cf569b737e6524438f3b1
Robert <robert@mobileappdevelopmentfirm.com>
2019-01-01 17:54:46 +0600

    plan subscription page HTML change

--- application/views/dashboard/plan-subscriptions-in.php
+++ application/views/dashboard/plan-subscriptions-in.php
@@ -9,30 +9,62 @@
 									1
 							  </div>
 							  <div class="card-body text-center">
-								 <h4 class="card-title">Silver Plan</h4>
+								 <h4 class="card-title">Bronze Plan</h4>
 							  </div>
 							  <div class="card-footer text-center">
-								 Set Up Fee : <b>$999.00</b>
+								 Set Up Fee : <b>$250.00</b>
 							  </div>
 							  <div class="card-footer text-center">
-								 Tablet : <b>$250.00</b>
-							  </div>
-							  <div class="card-footer text-center">
-								 Service Fee : <b>$129.00</b> <small>/ month*</small>
+								 Service Fee : <b>$150.00</b> <small>/ month*</small>
 							  </div>
 							  <div class="card-footer text-center">
 								 <small>*with 1 year contract</small>
 							  </div>
 							  <div class="clearfix"></div>
-							  <hr>
-									√ Multiple Page Questionnaire<br>
-									√ Custom Graphics<br>
-									√ Links to Yelp, Google+, Facebook, Trip Advisor, etc.<br>
-									√ Email Alerts if there is negative feedback<br>
-									√ Functionality Updates<br>
-									√ Spreadsheet Reporting
-									<br><br>
-									<hr>
+								<br><br>
+							 
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Attach to Invoice (Contractors) &nbsp;or Check (restaurants) or Check Out at Dr&rsquo;&rsquo;s &nbsp;Office Or Hotels</li>
+									<li class="list-group-item">Text goes out immediately to their phone</li>
+									<li class="list-group-item">Option to Leave Review on Google or FB &nbsp;only (Podium Model as Reference)</li>
+									<li class="list-group-item">Price : $250 Set Up fee &nbsp;$150/Month per location</li>
+								</ul>
+								<br><br>
+									<a href="<?=base_url('dashboard/page/plan_subscription_form?plan=silver')?>" type="submit" name="subs_submit2buy" class="btn btn-info btn-lg btn-block">Subscribe</a>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+						
+							  <div class="card-header text-center">
+									2
+							  </div>
+							  <div class="card-body text-center">
+								 <h4 class="card-title">Silver Plan</h4>
+							  </div>
+							  <div class="card-footer text-center">
+								 Set Up Fee (incudes Tablet): <b>$350.00</b>
+							  </div>
+							  <div class="card-footer text-center">
+								 Service Fee : <b>$250.00</b> <small>/ month*</small>
+							  </div>
+							  <div class="card-footer text-center">
+								 <small>*with 1 year contract</small>
+							  </div>
+							  <div class="clearfix"></div>
+								<br><br>
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Tablet with 5 Basic Question &ndash;Questionnaire on it presented in Office or Business or &nbsp;Home</li>
+									<li class="list-group-item">Positive Review opens link to leave review</li>
+									<li class="list-group-item">Negative Review pings Manager</li>
+									<li class="list-group-item">Text Reminder with Positive</li>
+									<li class="list-group-item">Email Reminder with Positive</li>
+									<li class="list-group-item">Incentive Gift Cards(lets discuss)</li>
+									<li class="list-group-item">Price $350 Set Up (incudes Tablet) &nbsp;$250.00/month per Location</li>
+								</ul>
+								<br><br>
 									<a href="<?=base_url('dashboard/page/plan_subscription_form?plan=silver')?>" type="submit" name="subs_submit2buy" class="btn btn-info btn-lg btn-block">Subscribe</a>
 						</div>
 					</div>
@@ -41,33 +73,35 @@
 					<div class="card ">
 						<div class="card-body">
 							  <div class="card-header text-center">
-									2
+									3
 							  </div>
 							  <div class="card-body text-center">
 								 <h4 class="card-title">Gold Plan</h4>
 							  </div>
 							  <div class="card-footer text-center">
-								 Set Up Fee: <b>$1799.00</b>
+								 Set Up Fee (incudes Tablet): <b>$550.00</b>
 							  </div>
 							  <div class="card-footer text-center">
-								 Tablet <b>$250.00</b>
-							  </div>
-							  <div class="card-footer text-center">
-								 Service Fee: <b>$249.00</b> <small>/ month*</small>
+								 Service Fee : <b>$250.00</b> <small>/ month*</small>
 							  </div>
 							  <div class="card-footer text-center">
 								 <small>*with 1 year contract</small>
 							  </div>
 							  <div class="clearfix"></div>
-							  <hr>
-									√ Custom Questionnaire<br>
-									√ Links to Yelp, Google+, Facebook, Trip Advisor, etc.<br>
-									√ Email Alerts if there is negative feedback<br>
-									√ Question Updates<br>
-									√ Custom Reports<br>
-									√ Granular Reporting Metrics<br>
-									√ Quarterly Design Edits
-									<hr>
+								<br><br>
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Tablet with 10 Custom Questions Question &ndash;Questionnaire on it presented in Office or Business or &nbsp;Home</li>
+									<li class="list-group-item">Positive Review opens link to leave review</li>
+									<li class="list-group-item">Negative Review pings Manager</li>
+									<li class="list-group-item">Text Reminder with Positive</li>
+									<li class="list-group-item">Email Reminder with Positive</li>
+									<li class="list-group-item">Survey Metrics for inner office management</li>
+									<li class="list-group-item">Social Media/Review sites Automated Updates</li>
+									<li class="list-group-item">Incentive Gift Cards (Lets Discuss)</li>
+									<li class="list-group-item">Price $550 Set Up (incudes Tablet) &nbsp;$250.00/month per Location</li>
+								</ul>
+								<br><br>
+							  
 									<a href="<?=base_url('dashboard/page/plan_subscription_form?plan=gold')?>" type="submit" name="subs_submit2buy" class="btn btn-info btn-lg btn-block">Subscribe</a>
 						</div>
 					</div>
